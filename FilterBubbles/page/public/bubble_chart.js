f28275e6391d91b6aec80143ca0a33a3893cf566
Jo10 <Johannes.Wiesneth@stud.uni-regensburg.de>
2018-06-11 22:09:53 +0200

    html and javascript

---
+++ FilterBubbles/page/public/bubble_chart.js
@@ -0,0 +1,149 @@
+/**
+ * Purpose:
+ * Generate a reusable bubble chart
+ *
+ * Instantiate the settings before rendering the bubble chart
+ * Generate a reusable bubble chart using d3.v4.js on a dataset loaded through D3.
+ *
+ * Original Author: Deborah Mesquita
+ * Source:
+ *
+ * {@link https://bl.ocks.org/dmesquita/37d8efdb3d854db8469af4679b8f984a Deborah Mesquita's block}
+ *
+ * {@link https://medium.freecodecamp.org/a-gentle-introduction-to-d3-how-to-build-a-reusable-bubble-chart-9106dc4f6c46 Tutorial and explanation}
+ *
+ * {@link https://taylorchasewhite.github.io/babyNames/ Live demo}
+ * @author Deborah Mesquita
+ * @author Taylor White <whitetc2@gmail.com>
+ * @since  07.04.17
+ * @summary  Generate a reusable bubble chart
+ * @requires d3.v4.js
+ * @class
+ *
+ * @example
+ * var chart = bubbleChart(); // instantiate the chart
+ *
+ * // update settings
+ * chart.width(850).height(850).minRadius(7).maxRadius(55).forceApart(-170);
+ *
+ * // example of chaining
+ * chart.columnForColors("Sex").columnForRadius("BirthCount");
+ * chart.customColors(["M","F"],["#70b7f0","#e76486"]).showTitleOnCircle(true);
+ * chart.title('Most popular baby names in 2016').columnForTitle("Name");
+ * chart.unitName("babies");
+ *
+ * // load the data and render the chart
+ * d3.select("#divBubbleChart")
+ *  .data(newData)
+ *  .call(chart);
+ *
+ * @returns Chart function so that you can render the chart when ready
+ */
+function bubbleChart() {
+    var width = 960,
+        height = 960,
+        maxRadius = 6,
+        columnForColors = "title",
+        columnForRadius = "value";
+
+    function chart(selection) {
+        var data = selection.datum();
+        var div = selection,
+            svg = div.selectAll('svg');
+        svg.attr('width', width).attr('height', height);
+
+        var tooltip = selection
+            .append("div")
+            .style("position", "absolute")
+            .style("visibility", "hidden")
+            .style("color", "white")
+            .style("padding", "8px")
+            .style("background-color", "#626D71")
+            .style("border-radius", "6px")
+            .style("text-align", "center")
+            .style("font-family", "monospace")
+            .style("width", "400px")
+            .text("");
+
+
+        var simulation = d3.forceSimulation(data)
+            .force("charge", d3.forceManyBody().strength([-50]))
+            .force("x", d3.forceX())
+            .force("y", d3.forceY())
+            .on("tick", ticked);
+
+        function ticked(e) {
+            node.attr("cx", function(d) {
+                    return d.x;
+                })
+                .attr("cy", function(d) {
+                    return d.y;
+                });
+        }
+
+        var colorCircles = d3.scaleOrdinal(d3.schemeCategory10);
+        var scaleRadius = d3.scaleLinear().domain([d3.min(data, function(d) {
+            return +d[columnForRadius];
+        }), d3.max(data, function(d) {
+            return +d[columnForRadius];
+        })]).range([5, 18])
+
+        var node = svg.selectAll("circle")
+            .data(data)
+            .enter()
+            .append("circle")
+            .attr('r', function(d) {
+                return scaleRadius(d[columnForRadius])
+            })
+            .style("fill", function(d) {
+                return colorCircles(d[columnForColors])
+            })
+            .attr('transform', 'translate(' + [width / 2, height / 2] + ')')
+            .on("mouseover", function(d) {
+                tooltip.html(d[columnForColors] + "<br>" + d[columnForRadius] + " Seiten");
+                return tooltip.style("visibility", "visible");
+            })
+            .on("mousemove", function() {
+                return tooltip.style("top", (d3.event.pageY - 10) + "px").style("left", (d3.event.pageX + 10) + "px");
+            })
+            .on("mouseout", function() {
+                return tooltip.style("visibility", "hidden");
+            });
+    }
+
+    chart.width = function(value) {
+        if (!arguments.length) {
+            return width;
+        }
+        width = value;
+        return chart;
+    };
+
+    chart.height = function(value) {
+        if (!arguments.length) {
+            return height;
+        }
+        height = value;
+        return chart;
+    };
+
+
+    chart.columnForColors = function(value) {
+        if (!arguments.columnForColors) {
+            return columnForColors;
+        }
+        columnForColors = value;
+        return chart;
+    };
+
+    chart.columnForRadius = function(value) {
+        if (!arguments.columnForRadius) {
+            return columnForRadius;
+        }
+        columnForRadius = value;
+        return chart;
+    };
+
+    return chart;
+}
+
---
+++ FilterBubbles/page/public/main.js
@@ -0,0 +1,120 @@
+Vue.config.productionTip = false;
+
+var Sidebar = Vue.component('sidebar', {
+    template: '<div class="col-2 bg-dark h-100"><ul class="nav flex-column pt-5"><template v-for="category in categories"><li class="nav-item"><a class="nav-link active" data-toggle="collapse" :href="[\'#collapseCategory\' + category.categoryName]" role="button" aria-expanded="false" aria-controls="collapseCategory">{{category.categoryName}}</a></li><li class="nav-item collapse ml-2" :id="[\'collapseCategory\' + category.categoryName]"><form class="text-white ml-4"><div v-for="subCat in category.subcategories" v-bind:key="subCat.subcategoryName" class="form-group form-check"><input type="checkbox" class="form-check-input" :value="subCat.subcategoryName" @change="onChange" v-model="checkedCats" :id="[\'subCat\' + subCat.subcategoryName]"><label class="form-check-label" :for="[\'subCat\' + subCat.subcategoryName]">{{subCat.subcategoryName}}</label></div></form></li></template></ul></div>',
+    props: ['categories'],
+    data: function() {
+        return {
+            checkedCats: [],
+        }
+    },
+    methods: {
+        onChange: function() {
+            this.$emit('catsChanged', this.checkedCats)
+        }
+    }
+});
+
+var MainContent = Vue.component('main-content', {
+    template: '<div class="col-10 h-100 bg-light"><div class="chart-example" id="chart"><svg></svg></div></div>',
+    props: ['categories', 'checkedCategories'],
+    watch: {
+        checkedCategories: function () {
+            this.data = this.mapCategoriesToBubbles(this.categories, this.checkedCategories);
+            var chart = bubbleChart().width(600).height(400);
+            d3.select('#chart').datum(this.data).call(chart);
+            return;
+        }
+    },
+    data: function() {
+        return {
+            data: null,
+        }
+    },
+    mounted() {
+        this.data = this.mapCategoriesToBubbles(this.categories, this.checkedCategories);
+        var chart = bubbleChart().width(600).height(400);
+        d3.select('#chart').datum(this.data).call(chart);
+    },
+    methods: {
+        mapCategoriesToBubbles: function(cats, checkedCats) {
+            let data = [];
+            data.push(this.getMyBubble(cats, checkedCats))
+            data = data.concat(this.getOtherBubbles(cats, checkedCats))
+            return data
+        },
+        getMyBubble: function(cats, checkedCats) {
+            let sites = []
+            for (var c = 0; c < cats.length; c++) {
+                for (var s = 0; s < cats[c].subcategories.length; s++) {
+                    for (var i = 0; i < checkedCats.length; i++) {
+                        if(checkedCats[i] == cats[c].subcategories[s].subcategoryName) {
+                            sites = sites.concat(cats[c].subcategories[s].sites)
+                        }
+                    }
+                }
+            }
+            return {
+                title: 'Meine Bubble',
+                value: sites.length,
+                category: 'sites'
+            }
+        },
+        getOtherBubbles: function(cats, checkedCats) {
+            let bubbles = [];
+            let hasSubcatChecked = false;
+            for (var c = 0; c < cats.length; c++) {
+                for (var s = 0; s < cats[c].subcategories.length; s++) {
+                    for (var i = 0; i < checkedCats.length; i++) {
+                        if(checkedCats[i] == cats[c].subcategories[s].subcategoryName) {
+                            hasSubcatChecked = true;
+                            break
+                        }
+                    }
+                    if(hasSubcatChecked) break
+                }
+                if (!hasSubcatChecked) {
+                    let sites = [];
+                    for (var s = 0; s < cats[c].subcategories.length; s++) {
+                        sites = sites.concat(cats[c].subcategories[s].sites)
+                    }
+                    let bubble = {
+                        title: cats[c].categoryName,
+                        value: sites.length,
+                        category: 'sites'
+                    };
+                    bubbles.push(bubble)
+                }
+                hasSubcatChecked = false;
+            }
+            return bubbles
+        },
+    },
+});
+
+var App = Vue.component('app', {
+  template: '<div id="app" class="row container-fluid h-100"><sidebar v-on:catsChanged="onCatsChanged" :categories="categories"></sidebar><main-content :categories="categories" :checkedCategories="checkedCats"></main-content></div>',
+  components: {
+    MainContent, Sidebar,
+  },
+  data: function() {
+      return {
+        categories: [],
+        checkedCats: [],
+      }
+    },
+    mounted() {
+        $.getJSON("./data.json", (json) => {
+            this.categories = json.categories;
+        });
+    },
+    methods: {
+      onCatsChanged: function(checkedCats) {
+        this.checkedCats = checkedCats;
+      },
+    },
+});
+
+new Vue({
+  render: h => h(App)
+}).$mount('#app');
